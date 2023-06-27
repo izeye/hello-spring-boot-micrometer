46fe9280458b0bde45a6aeec3c21349ac70312bb
Johnny Lim <izeye@naver.com>
2023-06-28 05:23:49 +0900

    Add hello world for Spring Boot Micrometer integration

---
+++ src/main/java/com/izeye/helloworld/springbootmicrometer/HelloSpringBootMicrometerApplication.java
@@ -0,0 +1,13 @@
+package com.izeye.helloworld.springbootmicrometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootMicrometerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringBootMicrometerApplication.class, args);
+	}
+
+}
