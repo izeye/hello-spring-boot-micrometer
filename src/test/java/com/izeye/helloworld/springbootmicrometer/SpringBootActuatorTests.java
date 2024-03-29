250f4f15f27f3fee727699ff7b6651c3e1289601
Johnny Lim <izeye@naver.com>
2024-03-29 10:17:15 +0900

    Downgrade to Spring Boot 3.3.0-M2
    Prometheus endpoint is not available with 3.3.0-M3.

---
+++ src/test/java/com/izeye/helloworld/springbootmicrometer/SpringBootActuatorTests.java
@@ -0,0 +1,26 @@
+package com.izeye.helloworld.springbootmicrometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+/**
+ * Tests for Spring Boot Actuator.
+ *
+ * @author Johnny Lim
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureObservability
+class SpringBootActuatorTests {
+
+    @Autowired
+    WebTestClient client;
+
+    @Test
+    void prometheus() {
+        this.client.get().uri("/actuator/prometheus").exchange().expectStatus().isOk();
+    }
+
+}
