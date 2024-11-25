302766e328c777847065b5da90491b83902e7cd4
Johnny Lim <izeye@naver.com>
2024-11-25 21:11:47 +0900

    Migrate to JCache backed by Ehcache 3

--- src/test/java/com/izeye/helloworld/springbootmicrometer/CacheMetricsTests.java
+++ src/test/java/com/izeye/helloworld/springbootmicrometer/CacheMetricsTests.java
@@ -1,6 +1,7 @@
 package com.izeye.helloworld.springbootmicrometer;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +24,7 @@ class CacheMetricsTests {
     @Autowired
     MeterRegistry meterRegistry;
 
+    @Disabled("See https://github.com/micrometer-metrics/micrometer/blob/9537358666e75c482815ca4cab632a44ce8c7a16/micrometer-core/src/main/java/io/micrometer/core/instrument/binder/cache/JCacheMetrics.java#L104-L108")
     @Test
     void cacheSize() {
         assertThat(this.meterRegistry.get("cache.size").tag("cache", "persons").gauge().value()).isEqualTo(0);
